ea9f8934e845ffdb0f74f5c8e7c439d756957539
Mudit Kapoor <muditk441@gmail.com>
2023-06-14 16:13:27 +0530

    backend hosted

--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -1,4 +1,5 @@
-export const host = "http://localhost:4000"
+// export const host = "http://localhost:4000"
+export const host = "https://backend-chat-app-drbx.onrender.com"
 export const registerRoute = `${host}/api/auth/register`
 export const loginRoute = `${host}/api/auth/login`
 export const setAvatarRoute = `${host}/api/auth/setAvatar`
